2896666e01387674446fb937b3f3b21e9b8304f4
Joshua Castaneda <joshua444jc@gmail.com>
2021-02-07 22:46:34 -0800

    Refactoring everything

--- index.js
+++ index.js
@@ -4,7 +4,4 @@ function Wifu(token, options) {
     return new Client(token, options);
 }
 
-Wifu.Command = require("./lib/structures/Command");
-Wifu.Intents = require("./lib/constants").INTENTS;
-
 module.exports = Wifu;
